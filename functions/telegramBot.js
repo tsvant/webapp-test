1bb2d3ef6fcd9e0d6c3523ecc34ff5e15a2140c5
Andrei <128181486+tsvant@users.noreply.github.com>
2023-03-26 01:32:59 +0800

    Create telegramBot.js

---
+++ functions/telegramBot.js
@@ -0,0 +1,13 @@
+
+exports.handler = async function (event) {
+  const body = JSON.parse(event.body);
+  const message = body.message || body.edited_message;
+
+  // Process the message and respond
+  // ...
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ status: 'OK' }),
+  };
+};
